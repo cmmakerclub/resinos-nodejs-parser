52db6c226082bd67a9788e24589df37b0e19f447
Nat <nat@cmmc.io>
2017-06-16 07:05:22 +0700

    remove serialport

--- app/index.js
+++ app/index.js
@@ -1,47 +1,6 @@
 #!/bin/env node
 
-{
+const chalk = require('chalk');
 
-  const chalk = require('chalk');
-  const SerialPort = require('serialport');
-  const port = new SerialPort(process.env.TARGET_PORT, {
-    baudRate: parseInt(process.env.TARGET_BAUDRATE)
-  });
 
-  SerialPort.list((err, ports) => {
-    'use strict';
-    console.log('\nList of serial interfaces:');
-    ports.forEach((port) => {
-      console.log(chalk.yellow(port.comName));
-      if (port.pnpId) {
-        console.log(chalk.yellow(port.pnpId));
-      }
-      if (port.manufacturer) {
-        console.log(chalk.yellow(port.manufacturer));
-      }
-    });
-    console.log('\n');
-  });
-
-  port.on('open', () => {
-    'use strict';
-    port.write(process.env.TEST_CMD, (err) => {
-      if (err) {
-        return console.log(chalk.red('Error on write: ', err.message));
-      }
-      console.log(chalk.magenta('message written'));
-    });
-  });
-
-  // open errors will be emitted as an error event
-  port.on('error', (err) => {
-    'use strict';
-    console.log(chalk.red('Error: ', err.message));
-  });
-
-  port.on('data', (data) => {
-    'use strict';
-    console.log(chalk.cyan('Data: ' + data));
-  });
-
-}
+console.log(`Hello world ${new Date()}`);
